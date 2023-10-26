5492f83b3cb1aff29272f1b50e6a5800eac91f23
jmmoline14 <jmmoline14@gmail.com>
2023-10-25 20:34:04 -0400

    encoder

---
+++ main.py
@@ -0,0 +1,29 @@
+#Joseph Moline
+
+
+def encode(to_be_encoded):
+
+    new_list = ''
+    for i in range(len(to_be_encoded)):
+
+        new_value = (3 + int(to_be_encoded[i]))%10
+        new_list += str(new_value)
+    print(new_list)
+    return new_list
+
+if __name__ == '__main__':
+
+    while True:
+        print('Menu \n-------------\n1. encode \n2. Decode \n3. Quit')
+        user_input = int(input('Please enter an option:'))
+        if user_input == 1:
+            to_be_encoded = str(input('Please enter a passcode to enter:'))
+            encoded_value = encode(to_be_encoded)
+            print('Your password has been encoded and stored!')
+        elif user_input == 3:
+            break
+        elif user_input == 2:
+            decode(encoded_value)
+            print(f'The encoded password is {encoded_value} and the original value password is {to_be_encoded} ')
+
+
